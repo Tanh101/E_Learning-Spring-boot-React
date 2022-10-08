435c855377b79d4186e8d1bdb74cf432679452e8
Van Tanh Ly <lyvantanh101@gmail.com>
2022-10-09 01:10:28 +0700

    done database

--- BE_Spring/src/main/java/com/crossguild/elearning/model/Card/Page.java
+++ BE_Spring/src/main/java/com/crossguild/elearning/model/Card/Page.java
@@ -1,6 +1,5 @@
 package com.crossguild.elearning.model.Card;
 
-import com.crossguild.elearning.model.quiz.Question;
 import com.crossguild.elearning.model.user.User;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -9,31 +8,25 @@ import lombok.ToString;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Set;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-public class Card {
+public class Page {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String title;
+    private String text;
+    private String textDescription;
 
-    private LocalDateTime createdDate;
-
-    @OneToMany(mappedBy = "card", fetch = FetchType.LAZY)
-    @EqualsAndHashCode.Exclude
-    @ToString.Exclude
-    private Set<Page> pages;
+    private String image;
 
     @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @JoinColumn(name = "card_id", referencedColumnName = "id")
     @EqualsAndHashCode.Exclude
     @ToString.Exclude
-    private User user;
+    private Card card;
 
 
 }
